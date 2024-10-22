35c6df9be8b82cf5b7f5a769f8ae67edbcced9d1
hyunwoo <kshsim98@naver.com>
2024-10-22 19:34:49 +0900

    아이템 등록 수정하는 중

--- src/main/java/jpaproject/jpa/dto/PasswordDTO.java
+++ src/main/java/jpaproject/jpa/dto/PasswordDTO.java
@@ -1,8 +1,6 @@
 package jpaproject.jpa.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,9 +9,9 @@ import lombok.Setter;
 public class PasswordDTO {
 
     @NotBlank(message = "비밀번호는 필수입니다.")
-    @Size(min = 8, max = 20, message = "비밀번호는 최소 8자리에서 20자리 사이입니다.")
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,20}$",
-        message = "최소 하나의 숫자,소문자,대문자,특수문자를 포함해야하며 공백이 없어야합니다.")
+//    @Size(min = 8, max = 20, message = "비밀번호는 최소 8자리에서 20자리 사이입니다.")
+//    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,20}$",
+//        message = "최소 하나의 숫자,소문자,대문자,특수문자를 포함해야하며 공백이 없어야합니다.")
     private String password;
 
     @NotBlank(message = "비밀번호 확인은 필수입니다.")
