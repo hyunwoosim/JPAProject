b726ea3ab7409bd260ec51b4189fc5391f0149f4
hyunwoo <kshsim98@naver.com>
2024-11-06 23:06:32 +0900

    수련중

--- src/main/java/jpaproject/jpa/JpaApplication.java
+++ src/main/java/jpaproject/jpa/JpaApplication.java
@@ -1,16 +1,21 @@
 package jpaproject.jpa;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaApplication {
 
 
+    public static void main(String[] args) {
+        SpringApplication.run(JpaApplication.class, args);
 
-	public static void main(String[] args) {
-		SpringApplication.run(JpaApplication.class, args);
+    }
 
-	}
-
+    @Bean
+    Hibernate5JakartaModule hibernate5Module() {
+        return new Hibernate5JakartaModule();
+    }
 }
---
+++ src/main/java/jpaproject/jpa/api/OrderSimpleApiController.java
@@ -0,0 +1,28 @@
+package jpaproject.jpa.api;
+
+import java.util.List;
+import jpaproject.jpa.domain.Order;
+import jpaproject.jpa.repository.OrderRepository;
+import jpaproject.jpa.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * xToOne(ManyToOne, OneToOne)
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        return all;
+    }
+}
